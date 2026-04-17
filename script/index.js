afe8dc56261c6d00be6ea0e5390a9a502644ca07
tildanyrvik <tilda.nyrvik@gmail.com>
2026-04-17 20:15:40 +0200

    markAsWatched function created

--- script/index.js
+++ script/index.js
@@ -3,21 +3,22 @@ while (running) {
             Movie tracker
             1. Add Movie
             2. List Movies
-            3. Mark Movie as Read
+            3. Mark Movie as Watched
             4. Exit
             Enter your choice:`);
     
 
     switch (choice) {
         case "1":
-            addMovie();
+            const title = prompt("Enter the title of the movie to add to library");
+            addMovie(title);
             break;
         case "2":
             listMovies();
             break;
         case "3":
-            const title = prompt("Enter the title of the movie to mark as read");
-            markAsRead(title);
+            const title = prompt("Enter the title of the movie to mark as watched");
+            markAsWatched(title);
             break;
         case "4":
             running = false;
@@ -28,5 +29,41 @@ while (running) {
     }    
 }
 
+const library = [
+    {
+        title: "Taxi Driver",
+        director: "Martin Scorsese",
+        isWatched: false
+    },
+    {
+        title: "Midsommar",
+        director: "Ari Aster",
+        isWatched: false
+    },
+    {
+        title: "Whiplash",
+        director: "Damien Chazelle",
+        isWatched: true
+    },
+    {
+        title: "The Shining",
+        director: "Stanley Kubrick",
+        isWatched: true 
+    }
+];
 
 
+function addMovie(title) {
+    library.push({
+        title: title,
+        isWatched: false,
+    });
+}
+
+addMovie(title);
+
+function listMovies() {
+    library.forEach(movie => console.log(movie.title, movie.director, movie.isWatched));
+}
+
+listMovies();
